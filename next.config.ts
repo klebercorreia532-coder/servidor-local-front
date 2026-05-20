6bf5bfe41f2fa1cb2e8aa1c767cf04e404aa93b2
Kleber Veiga <Klebercorreia532@gmail.com>
2026-05-20 12:40:51 -0100

    primeiro comit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   images: {
-    domains: ["images.unsplash.com", "plus.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
   },
-  /* config options here */
-
 };
 
 export default nextConfig;
